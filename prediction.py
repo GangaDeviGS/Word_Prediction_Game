ab5ec533b804b7ad8cfd9f40b5754e459cea522d
Ganga Devi G S <gangavgpotti@gmail.com>
2023-10-04 15:38:33 +0530

    Added some comments for better understanding

--- prediction.py
+++ prediction.py
@@ -29,7 +29,7 @@ while turns > 0:
 # In while loop create 'for loop' 
 # For identify each charachter in the random word   
     for char in word:
-# Here creating another 'for loop '
+# Here creating  'if condition '
 # For identifying guess word is correct or not         
         if char in guesses:
             print (char, end =" ") 
@@ -41,19 +41,25 @@ while turns > 0:
 # And the game end            
     if failed == 0:
         print ("YOU WIN !!!")
-        print ( "The word is", word) 
+        print ("\n")
+        print ( "\t The word is", word) 
         break 
 # If user has input the wrong alphabet then
 # it will ask user to enter another alphabet         
 
     print()
-   
+# Here the user is asked to enter character   
     guess = input ("guess a character : ") 
+# Every input character will be stored in guesses
     guesses += guess
-
+# Check input with the character in word
     if guess not in word :
-        turns -= 1   
+        turns -= 1  
+# If the character doesn’t match the word
+# Then “Wrong” will be given as output         
         print ("Worng Try another one")
+# This will print the number of
+# turns left for the user        
         print ( "You have", + turns, "more guesses")
         if turns == 0:      
             print("You Loose")
